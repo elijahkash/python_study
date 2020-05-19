10dee5e0afd0f4522d2eddead8f1a8b8fc6ca65e
elijah <elijahkashnitskiy@gmail.com>
2020-05-19 12:21:12 +0300

    add sh for clean python caches

--- clean_tmp.sh
+++ clean_tmp.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+find . -type d -name __pycache__  -not -path "./venv/*" -exec rm -rf {} \;
+
+find . -type d -name .mypy_cache  -not -path "./venv/*" -exec rm -rf {} \;
+
+find . -type d -name .pytest_cache  -not -path "./venv/*" -exec rm -rf {} \;
