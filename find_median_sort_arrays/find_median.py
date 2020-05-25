4dee6d34914288f9b2d06606465c7c7e2ae21919
elijah <elijahkashnitskiy@gmail.com>
2020-05-25 15:11:45 +0300

    solve with O(m)

--- find_median_sort_arrays/find_median.py
+++ find_median_sort_arrays/find_median.py
@@ -6,16 +6,50 @@ class Solution:
                                nums2: List[int]) -> float:
         l1 = len(nums1)
         l2 = len(nums2)
-        if len(nums2) > len(nums1):
+        if l2 > l1:
             nums1, nums2 = nums2, nums1
             l1, l2 = l2, l1
-        nums1.append(nums1[-1])
-        nums2.append(nums2[-1])
-        y = l2
-        x = (l1 - l2) // 2
-        while True:
-            if nums1[x] <= nums2[y + 1] and nums2[y] <= nums1[x + 1]:
-                break
-            y -= 1
-            x += 1
-        return x, y
+        if l2 == 0:
+            if l1 % 2:
+                return nums1[l1 // 2]
+            else:
+                return (nums1[l1 // 2] + nums1[l1 // 2 - 1]) / 2
+        d = (l1 - l2) // 2
+        isOdd = bool((l1 + l2) % 2)
+        # y == 0
+        if nums1[l1 - d - 1 - (1 if isOdd else 0)] <= nums2[0]:
+            if l1 == l2:
+                return ((nums1[-1] + nums2[0]) / 2)
+            elif isOdd:
+                return min(nums1[l1 - d - 1], nums2[0])
+            else:
+                return ((nums1[l1 - d - 1] + min(nums2[0], nums1[l1 - d])) / 2)
+        # y = l2
+        elif nums1[d] >= nums2[-1]:
+            if l1 == l2:
+                return ((nums2[-1] + nums1[0]) / 2)
+            elif isOdd:
+                return nums1[d]
+            else:
+                return ((nums1[d] + max(nums2[-1], nums1[d - 1])) / 2)
+        # x = [d + 1: l1 - 1]; y = [1: l2 - 1]
+        else:
+            x = l1 - d
+            if isOdd:
+                x -= 1
+            for y in range(1, l2):
+                x -= 1
+                rval = min(nums1[x], nums2[y])
+                lval = max(nums1[x - 1], nums2[y - 1])
+                if isOdd:
+                    if rval >= lval:
+                        return rval
+                    else:
+                        continue
+                else:
+                    if rval >= lval:
+                        return (rval + lval) / 2
+                    else:
+                        continue
+
+        raise NameError('Shit!')
--- find_median_sort_arrays/test_findmedian.py
+++ find_median_sort_arrays/test_findmedian.py
@@ -1,13 +1,60 @@
 from find_median import Solution
 
 
+
 def test_1():
-	assert(Solution.findMedianSortedArrays(None, [1, 3], [2]) == 2)
-
-
+	assert(Solution.findMedianSortedArrays(None, [], [4]) == 4)
 def test_2():
-	assert(Solution.findMedianSortedArrays(None, [1, 2], [3, 4]) == 2.5)
-
-
+	assert(Solution.findMedianSortedArrays(None, [], [-1, 2]) == 0.5)
 def test_3():
-	assert(Solution.findMedianSortedArrays(None, [1, 3, 9], [0]) == 3)
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 6], []) == 3)
+def test_4():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 6, 9], []) == 4.5)
+def test_5():
+	assert(Solution.findMedianSortedArrays(None, [3], [2]) == 2.5)
+def test_6():
+	assert(Solution.findMedianSortedArrays(None, [2], [5]) == 3.5)
+def test_7():
+	assert(Solution.findMedianSortedArrays(None, [3, 4], [2]) == 3)
+def test_8():
+	assert(Solution.findMedianSortedArrays(None, [3, 5], [7]) == 5)
+def test_9():
+	assert(Solution.findMedianSortedArrays(None, [3, 7], [4]) == 4)
+def test_10():
+	assert(Solution.findMedianSortedArrays(None, [3, 5, 7], [2]) == 4)
+def test_11():
+	assert(Solution.findMedianSortedArrays(None, [3, 4, 6], [9]) == 5)
+def test_12():
+	assert(Solution.findMedianSortedArrays(None, [4, 6, 9], [7]) == 6.5)
+def test_13():
+	assert(Solution.findMedianSortedArrays(None, [1, 7, 8], [3]) == 5)
+
+
+def test_14():
+	assert(Solution.findMedianSortedArrays(None, [1, 2], [3, 4]) == 2.5)
+def test_15():
+	assert(Solution.findMedianSortedArrays(None, [5, 7], [3, 4]) == 4.5)
+def test_16():
+	assert(Solution.findMedianSortedArrays(None, [1, 3], [2, 4]) == 2.5)
+def test_17():
+	assert(Solution.findMedianSortedArrays(None, [5, 7], [1, 6]) == 5.5)
+def test_18():
+	assert(Solution.findMedianSortedArrays(None, [1, 5, 7], [0, 4, 9]) == 4.5)
+
+def test_20():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 9], [-1, 0]) == 1)
+def test_21():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 5], [7, 9]) == 5)
+def test_22():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 9], [2, 6]) == 3)
+
+def test_23():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 5, 8, 9, 11, 13], [-1, 4, 7, 12]) == 7)
+def test_24():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 5, 8, 9, 11], [4, 7, 12]) == 7)
+def test_25():
+	assert(Solution.findMedianSortedArrays(None, [1, 3, 7, 8, 9, 11], [4, 5, 12]) == 7)
+def test_92():
+	assert(Solution.findMedianSortedArrays(None, [3, 5, 7, 9], [6]) == 6)
+
+# test_11()
